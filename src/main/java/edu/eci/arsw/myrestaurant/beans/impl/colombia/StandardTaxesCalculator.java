73872a06d3a2adda770d1060c5aff8f1f8f56c91
unknown <yeltzyn.sierra-a@labinfo.is.escuelaing.edu.co>
2025-04-03 09:15:53 -0500

    primeros dos puntos

--- src/main/java/edu/eci/arsw/myrestaurant/beans/impl/colombia/StandardTaxesCalculator.java
+++ src/main/java/edu/eci/arsw/myrestaurant/beans/impl/colombia/StandardTaxesCalculator.java
@@ -3,7 +3,12 @@ package edu.eci.arsw.myrestaurant.beans.impl.colombia;
 import org.springframework.stereotype.Service;
 import edu.eci.arsw.myrestaurant.model.RestaurantProduct;
 import edu.eci.arsw.myrestaurant.beans.TaxesCalculator;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
 
+
+@Component
+@Qualifier("Standard")
 public class StandardTaxesCalculator implements TaxesCalculator {
 
 	@Override
