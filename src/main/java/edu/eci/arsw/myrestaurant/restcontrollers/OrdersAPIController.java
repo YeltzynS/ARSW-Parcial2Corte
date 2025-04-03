bdbfc54b944b5a734a505b24bb25774eb212187c
unknown <yeltzyn.sierra-a@labinfo.is.escuelaing.edu.co>
2025-04-03 09:27:17 -0500

    primeros dos puntos terminados

--- src/main/java/edu/eci/arsw/myrestaurant/restcontrollers/OrdersAPIController.java
+++ src/main/java/edu/eci/arsw/myrestaurant/restcontrollers/OrdersAPIController.java
@@ -17,26 +17,18 @@
 package edu.eci.arsw.myrestaurant.restcontrollers;
 
 import edu.eci.arsw.myrestaurant.model.Order;
-import edu.eci.arsw.myrestaurant.model.ProductType;
-import edu.eci.arsw.myrestaurant.model.RestaurantProduct;
 import edu.eci.arsw.myrestaurant.services.OrderServicesException;
 import edu.eci.arsw.myrestaurant.services.RestaurantOrderServices;
-import edu.eci.arsw.myrestaurant.services.RestaurantOrderServicesStub;
-import java.util.Hashtable;
-import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
 
 
 /**
@@ -65,22 +57,20 @@ public class OrdersAPIController {
 
     private String getOrders() throws OrderServicesException{
         Set<Integer> orders = orderServices.getTablesWithOrders();
-        Object[] orderP = orders.toArray();
+        Object[] orderRest = orders.toArray();
         Gson gson = new Gson();
-        String response = "[";
-        for(int i = 0; i < orderP.length; i++){
-            Order order = orderServices.getTableOrder((Integer) orderP[i]);
+        String response = "The orders are";
+        for(int i = 0; i < orderRest.length; i++){
+            Order order = orderServices.getTableOrder((Integer) orderRest[i]);
             JsonElement jsonElement = gson.toJsonTree(order);
             jsonElement.getAsJsonObject().addProperty("price", orderServices.calculateTableBill(order.getTableNumber()));
             response += gson.toJson(jsonElement);
-            if(i != orderP.length - 1){
+            if(i != orderRest.length - 1){
                 response += ",";
-            }else if(i == orderP.length - 1){
+            }else if(i == orderRest.length - 1){
                 response += "]";
             }
         }
         return response;
     }
 }
-    
-
